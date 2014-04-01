40646293f01a70e7ba17021cda747b5e0eab1e4f
Indamix <dvetreti@gmail.com>
2014-04-02 02:18:33 +0400

    updated Router

--- router.js
+++ router.js
@@ -2,15 +2,24 @@ var Events = require('events');
 
 var Router = Events.extend({
 
-  constructor: function Router() {
+  constructor: function Router(routes) {
     Events.call(this);
     this.onChange = this.onChange.bind(this);
     window.addEventListener('hashchange', this.onChange, false);
     setTimeout(this.onChange, 0);
+    if (routes) this.routes = routes;
   },
 
   onChange: function () {
-    this.trigger('route', location.hash.substr(1));
+    var url = location.hash.substr(2),
+        route, match;
+
+    for (var i = 0; i < this.routes.length; ++i) {
+      route = this.routes[i];
+      if (match = url.match(route[0])) break;
+    }
+
+    if (match) this.trigger('route', route[1], match.slice(1));
   }
 
 });
