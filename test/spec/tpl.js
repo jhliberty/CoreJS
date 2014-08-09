3025f0fc76c0266a677a8af5597fedfd32fc02fa
Indamix <dvetreti@gmail.com>
2014-08-10 01:03:30 +0400

    tpl: added test case for template with spaces and newlines (closes #2)

--- test/spec/tpl.js
+++ test/spec/tpl.js
@@ -14,6 +14,17 @@ describe('tpl', function(){
     '</div>' +
   '{{/items}}';
 
+  var tplWithSpacesAndNewlines =
+  '{{#items}}\
+    <div>\
+    {{title}}\
+    <ul>\
+      {{#items}}<li>{{.}}</li>{{/items}}\
+    </ul>\
+    [{{#test}}<p>&nbsp;&nbsp;&nbsp;&nbsp;-{{.}}</p>{{/test}}]\
+    </div>\
+  {{/items}}';
+
   var model = {
     items: [
       {
@@ -35,6 +46,12 @@ describe('tpl', function(){
   it('should render like Mustache does', function () {
     var html_i = tpl(template, model),
         html_m = mus(template, model);
+    expect(html_i).toBe(html_m);
+  });
+
+  it('should render templates with spaces and newlines', function () {
+    var html_i = tpl(tplWithSpacesAndNewlines, model),
+        html_m = mus(tplWithSpacesAndNewlines, model);
     expect(html_i).toBe(html_m);
   });
 });
